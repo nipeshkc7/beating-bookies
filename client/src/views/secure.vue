8e3b7549ba12afefc92533d7d066c75292d218f5
nipeshkc7 <nipeshkc7@gmail.com>
2020-02-28 18:40:21 +1100

    added tile-placeholders

--- client/src/views/secure.vue
+++ client/src/views/secure.vue
@@ -11,7 +11,7 @@
           </article>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child box  notification is-success">
+          <article class="tile is-child box notification is-success">
             <p class="title">Total profits</p>
             <p class="subtitle">{{ total_profits }}</p>
           </article>
@@ -21,6 +21,55 @@
             <p class="title">Biggest win this week</p>
             <p class="subtitle">{{ biggest_win }}</p>
           </article>
+        </div>
+      </div>
+      <div class="tile is-ancestor">
+        <div class="tile is-8 is-vertical">
+          <div class="tile is-parent">
+            <article class="tile is-child box is-info">
+              <p class="title">Your bets..</p>
+              <br/>
+              <p class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum
+                volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus,
+                leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis
+                ut quam.
+                Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula
+                a sagittis.
+                 Pellentesque
+                interdum, nisl nec interdum maximus, augue diam porttitor lorem, et
+                sollicitudin felis
+                 neque sit
+                amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit
+                sit amet.
+                 Aenean vitae
+                gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat
+                tortor.
+                Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae
+                congue
+                lectus dolor
+                consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a
+                metus.
+                Cras ullamcorper
+                a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non
+                semper leo.
+              </p>
+            </article>
+          </div>
+        </div>
+        <div class="tile is-4 is-vertical">
+          <div class="tile is-parent">
+            <article class="tile is-child box notification is-info">
+              <p class="title">To Do : Settle these bets !</p>
+              <p class="subtitle">{{ average_conversion }}</p>
+            </article>
+          </div>
+          <!-- <div class="tile is-parent">
+            <article class="tile is-child box notification is-info">
+              <p class="title">Average conversion</p>
+              <p class="subtitle">{{ average_conversion }}</p>
+            </article>
+          </div> -->
         </div>
       </div>
     </div>
@@ -70,11 +119,11 @@ export default {
 </script>
 
 <style scoped>
-.main-content{
-  padding:25px 0px;
+.main-content {
+  padding: 25px 0px;
 }
 
-.subtitle{
-  font-size: '90px',
+.subtitle {
+  font-size: "90px";
 }
 </style>
