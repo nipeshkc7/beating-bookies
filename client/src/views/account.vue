0bc4cacfe8d2456d50e5d7f8da405ff9d1343164
nipeshkc7 <nipeshkc7@gmail.com>
2020-04-03 23:35:02 +1100

    added account view

---
+++ client/src/views/account.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <b-field label="Name">
+      <b-input v-model="name"></b-input>
+    </b-field>
+    <b-field label="Email">
+      <b-input v-model="email"></b-input>
+    </b-field>
+    <b-field label="password">
+      <b-input v-model="name"></b-input>
+    </b-field>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'account',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+    };
+  },
+};
+</script>
